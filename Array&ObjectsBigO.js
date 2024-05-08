61931310c571ba5c32bd17b999b632c461d07e8b
zohaib <zohaibshafiq2005@gmail.com>
2024-05-08 11:17:41 +0500

    object and its methods Big O

---
+++ Array&ObjectsBigO.js
@@ -0,0 +1,10 @@
+let names = ["Michael", "Melissa", "Andrea"];
+
+let values = [true, {}, [], 2, "awesome"];
+
+let nums = [1, 2, 3, 4, 5];
+
+// console.log(nums.splice(2, 2, "HTML", "CSS"));
+// console.log(nums);
+
+let months = ['Jan']
